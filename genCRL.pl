f5cc73824e3d77c2ae35bcde7948c09995bd8e75
jchakma <jchakma@gmail.com>
2019-02-17 19:39:39 +0900

    Info related and sample update

--- genCRL.pl
+++ genCRL.pl
@@ -4,7 +4,14 @@
 # A simple script to generate CRL
 #
 
-# generate CRL
+# generate Root CRL
+print "openssl ca -gencrl -config root-ca.conf -out root-ca.crl\n";
+system("openssl ca -gencrl -config root-ca.conf -out root-ca.crl");
+
+# output in text format
+system("openssl crl -in root-ca.crl -noout -text");
+
+# generate Sub CA CRL
 print "openssl ca -gencrl -config sub-ca.conf -out sub-ca.crl\n";
 system("openssl ca -gencrl -config sub-ca.conf -out sub-ca.crl");
 
